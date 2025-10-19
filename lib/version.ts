4e6ae176ff8e1c23985d92c8ce381b142e85010b
Mgs <akerberganders@gmail.com>
2025-10-19 20:07:08 +0200

    gh pages

--- lib/version.ts
+++ lib/version.ts
@@ -9,7 +9,7 @@ export const VERSION = {
   // Static version info
   major: 1,
   minor: 0,
-  patch: 14,
+  patch: 15,
   // Get full version string
   get full() {
     return `${this.major}.${this.minor}.${this.patch}-${this.hash}`;
